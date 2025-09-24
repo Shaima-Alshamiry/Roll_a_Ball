de401f0e51cc0136a9eb5e9c65662c3c75e584ef
Shaima-Alshamiry <95910886+Shaima-Alshamiry@users.noreply.github.com>
2025-09-24 11:34:07 +0300

    Initial commit

---
+++ Assets/script/playerController.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Leap;
+
+public class playerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    private int count;
+    public TextMeshProUGUI countText;
+    public TextMeshProUGUI winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
